8bf3f3acdc23864b023cbf0019e0220381027826
Jesus <4186790@alu.murciaeduca.es>
2024-01-25 08:04:36 +0100

    Relaciones

--- app/Models/Ciclo.php
+++ app/Models/Ciclo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ciclo extends Model
 {
@@ -17,4 +18,9 @@ class Ciclo extends Model
         'grado',
         'nombre'
     ];
+
+    public function familiaProfesional(): BelongsTo
+    {
+        return $this->belongsTo(FamiliaProfesional::class, 'familia_id');
+    }
 }
